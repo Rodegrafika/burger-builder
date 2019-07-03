0148792f564ec6b5e7e7b96cb841cb086cf72669
Vanessa Rodegher <info@rodegrafika.de>
2019-07-03 15:27:20 +0200

    Updates

---
+++ precache-manifest.88e34409b6d5ab98aaf6ea2c7f969483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/./static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "75619e39c399be2d5a0c",
+    "url": "/burger-builder/./static/js/runtime~main.75619e39.js"
+  },
+  {
+    "revision": "49240e07d2fcc7dc6771",
+    "url": "/burger-builder/./static/js/main.49240e07.chunk.js"
+  },
+  {
+    "revision": "ca003b847ec5b6ccb977",
+    "url": "/burger-builder/./static/js/1.ca003b84.chunk.js"
+  },
+  {
+    "revision": "49240e07d2fcc7dc6771",
+    "url": "/burger-builder/./static/css/main.ac9a1507.chunk.css"
+  },
+  {
+    "revision": "111a914364c08a02ebb8e2da5a9f1de3",
+    "url": "/burger-builder/./index.html"
+  }
+];
